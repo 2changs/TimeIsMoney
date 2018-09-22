935661b4c8a95b023f82730e222a82b31bb19dc1
2changs <diana.chang05@gmail.com>
2018-09-22 19:28:58 -0400

    Use template

--- src/components/Card.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './Card.css';
-
-const Card = ({ zIndex = 0, children }) => (
-  <div className="Card" style={{ zIndex }}>{children}</div>
-);
-
-export default Card;
